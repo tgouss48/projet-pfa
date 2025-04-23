b49ccf062487b1912e767272a9937828bd51610e
tgouss48 <city4ard@gmail.com>
2025-04-23 02:05:48 +0100

    Modification

--- auth-api/index.js
+++ auth-api/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
--- upload-api/server.js
+++ upload-api/server.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connecté à MongoDB Atlas");
-    app.listen(process.env.PORT, () => {
-      console.log(`Serveur démarré on port : ${process.env.PORT}`);
-    });
+    app.listen(process.env.PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });    
   })
   .catch((err) => {
     console.error("Erreur connexion MongoDB :", err);
